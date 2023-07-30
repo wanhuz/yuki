71ba3957431a5fbbb223a8d630adb87bf64d35be
wan <wanhuz96@gmail.com>
2023-07-30 10:50:49 +0800

    Reuse old command

--- module/remote.py
+++ module/remote.py
@@ -1,5 +1,6 @@
 import subprocess
 import logging
+import time
 
 logging.basicConfig(
     filename="yuki.log", 
@@ -11,15 +12,20 @@ class Remote:
 
     @staticmethod
     def copyto(path_to_file, path_to_dest):
-        logging.info('Rclone copying ' + path_to_file + " to " + path_to_dest)
 
         copy_rclone_command = Remote.generate_rclone_copy_commands(path_to_file, path_to_dest)
-        subprocess.run(copy_rclone_command)
+        p = subprocess.Popen(copy_rclone_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+
+        while p.poll() is None:
+            time.sleep(1)
+
+        if (p.returncode):
+            logging.debug("Error, : " + "rclone copto" + " exited with non-zero status, filename: " + path_to_file + ", errorcode: " + str(p.returncode))
+            logging.debug("STDOUT: " + str(p.stdout))
+            logging.debug("Stderr:" + str(p.stderr))
+            logging.debug("Rclone command: " + "' " + copy_rclone_command + " '")
 
     @staticmethod
     def generate_rclone_copy_commands(path_to_file, path_to_dest):
-        path_to_file = path_to_file
-        path_to_dest =  path_to_file
-        log_command = "--log-file=yuki_rclone.log"
-        copy_rclone_command = ['rclone', 'copyto', str(path_to_file), str(path_to_dest), str(log_command)]
+        copy_rclone_command = "rclone copyto " + "'" + path_to_file + "' " + "'" + path_to_dest + "'" + " --log-file=yuki_rclone.log"
         return copy_rclone_command
