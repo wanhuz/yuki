5b0100e0a87b4bf56760356eebc9f0aaf6aedcb6
wan <wanhuz96@gmail.com>
2023-07-31 14:26:03 +0800

    add help commands in cli

--- yuki.py
+++ yuki.py
@@ -1,7 +1,12 @@
 import sys
 from module.controller import Controller
 
-def main():
+def main(*args):
+
+    if (len(sys.argv) == 1):
+        print('Invalid commands! Run with --help to view all commands')
+        return
+    
     args = sys.argv[1:]
 
     if (args[0] == "run"):
@@ -10,8 +15,10 @@ def main():
     elif (args[0] == "cli"):
         controller = Controller()
         controller.interactive()
+    elif (args[0] == "--help"):
+        print('Arguments: \n\nrun - execute yuki script\ncli - enter interactive session to view records, add or delete')
     else:
-        print('Invalid commands!')
+        print('Invalid commands! Run with --help to view all commands')
 
 if __name__ == "__main__":
     main()
