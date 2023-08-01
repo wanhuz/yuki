47983dd3db17781a1dd1ffdbbbaaad05e0210b81
wan <wanhuz96@gmail.com>
2023-08-01 21:03:48 +0800

    Fix typo

--- tests/test_util.py
+++ tests/test_util.py
@@ -19,11 +19,11 @@ class TestUtil(unittest.TestCase):
         invalid_filename = "photo"
         self.assertFalse(Util.get_extension(invalid_filename))
 
-    def test_numbered_get_extension_filename(self):
+    def test_get_extension_numbered_filename(self):
         valid_filename = "23123123.png"
         self.assertEqual(Util.get_extension(valid_filename), ".png")
 
-    def test_japanese_get_extension_filename(self):
+    def test_get_extension_japanese_filename(self):
         japanese_filename = "わたしわ.mkv"
         self.assertEqual(Util.get_extension(japanese_filename), ".mkv")
 
