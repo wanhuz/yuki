cdf09ca988340eb8622379942b9fa8a58bed5e49
wan <wanhuz96@gmail.com>
2026-04-26 17:22:22 +0800

    Add maximum allowable instance for linux

--- module/controller.py
+++ module/controller.py
@@ -4,6 +4,11 @@ from module.cli import Cli
 import logging
 from dotenv import load_dotenv
 import os
+import sys
+
+if sys.platform.startswith("linux"):
+    import fcntl
+    import atexit
 
 logging.basicConfig(
     filename="yuki.log", 
@@ -15,6 +20,16 @@ class Controller:
 
     def __init__(self, dry_run = False):
         load_dotenv()
+
+        if sys.platform.startswith("linux"):
+            self.lock_file_handle = None
+            max_active_instance = int(os.getenv("MAX_ACTIVE_INSTANCE", 10))
+            
+            if not self.acquire_slot(MAX_INSTANCES=max_active_instance):
+                print("Max instances reached, exiting")
+                sys.exit(1)
+
+            atexit.register(self.release_slot)
 
         record_path = os.getenv("RECORD_PATH")
         src_path = os.getenv("SRC_PATH")
@@ -50,3 +65,29 @@ class Controller:
 
     def interactive(self):
         self.cli.start()
+
+    def acquire_slot(self, LOCK_DIR="./locks", MAX_INSTANCES=4):
+        os.makedirs(LOCK_DIR, exist_ok=True)
+
+        for i in range(MAX_INSTANCES):
+            path = f"{LOCK_DIR}/instance_{i}.lock"
+
+            try:
+                f = open(path, "w")
+                fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
+
+                self.lock_file_handle = f
+                return True
+
+            except IOError:
+                continue
+
+        return False
+    
+    def release_slot(self):
+        if self.lock_file_handle:
+            try:
+                fcntl.flock(self.lock_file_handle, fcntl.LOCK_UN)
+                self.lock_file_handle.close()
+            except:
+                pass
