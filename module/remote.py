9550c82f401e88246ed0e82b9bffca79e23dc3d9
wan <wanhuz96@gmail.com>
2026-05-25 14:38:10 +0800

    Remove staging move

--- module/remote.py
+++ module/remote.py
@@ -17,18 +17,13 @@ class Remote:
 
     @staticmethod
     def copyto(record: Record, file_id, path_to_file, path_to_dest, file_transfer_tool="rsync"):
-        load_dotenv()
-        staging_root = os.getenv("TMP_PATH")
-        os.makedirs(os.path.dirname(staging_root), exist_ok=True) # Untested
-        
-        staging_dest = os.path.join(staging_root, os.path.basename(path_to_dest))
-        
+
         
         match file_transfer_tool:
             case "rsync":
-                copy_command = Remote.generate_rsync_copy_commands(path_to_file, staging_dest)
+                copy_command = Remote.generate_rsync_copy_commands(path_to_file, path_to_dest)
             case "rclone":
-                copy_command = Remote.generate_rclone_copy_commands(path_to_file, staging_dest)
+                copy_command = Remote.generate_rclone_copy_commands(path_to_file, path_to_dest)
             case _:
                 logging.debug("Unknown file transfer tool: " + file_transfer_tool)
                 record.set_error(file_id, "Unknown file transfer tool: " + file_transfer_tool)
@@ -68,7 +63,6 @@ class Remote:
             return
         else:
             logging.info(f"Copied {path_to_file} to {path_to_dest}")
-            Remote.promote_to_final(staging_dest, path_to_dest)
             record.mark_as_finished(file_id)
             return
 
@@ -79,6 +73,12 @@ class Remote:
             "copy",
             path_to_file,
             path_to_dest,
+            "--multi-thread-streams=4",
+            "--transfers=4",
+            "--multi-thread-cutoff=64M",
+            "--retries=10",
+            "--low-level-retries=20",
+            "--timeout=15m",
             "--log-file=yuki_rclone.log"
         ]
     
@@ -97,18 +97,3 @@ class Remote:
             path_to_dest
         ]
 
-    @staticmethod
-    def promote_to_final(staging_path, final_path):
-        directory = os.path.dirname(final_path)
-
-        if directory:
-            os.makedirs(directory, exist_ok=True)
-
-        try:
-            os.replace(staging_path, final_path)
-        except OSError:
-            try:
-                shutil.move(staging_path, final_path)
-            except Exception:
-                logging.error(f"Failed to move {staging_path} to {final_path}")
-                raise
